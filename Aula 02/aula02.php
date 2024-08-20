93a5f2fea4af9a9a057ab5db43c7e3299e15f5a2
DavyAndrade <hallszindementahhh@gmail.com>
2024-08-20 16:46:50 -0300

    aula_02

--- Aula 02/aula02.php
+++ Aula 02/aula02.php
@@ -22,7 +22,7 @@ if ($tipo == 'soma') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculadora Básica</title>
 </head>
 
 <body>
